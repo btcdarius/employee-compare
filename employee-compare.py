b45998468d1bac2316343ecadadd2f79f0da0abc
Brian Craine <briancraine@macbook-air.local>
2022-08-22 02:47:50 +0000

    Began implementing actual column names

--- employee-compare.py
+++ employee-compare.py
@@ -3,14 +3,14 @@ import csv
 
 # Define the Employee class
 class Employee:
-	def __init__(self, id, name, department, birthMonth):
-		self.id = id
-		self.name = name
+	def __init__(self, employeeNumber, department, group, team):
+		self.employeeNumber = employeeNumber
 		self.department = department
-		self.birthMonth = birthMonth
+		self.group = group
+		self.team = team
 
 	def __eq__(self, other):
-		return self.id == other.id and self.name == other.name and self.department == other.department and self.birthMonth == other.birthMonth
+		return self.employeeNumber == other.employeeNumber and self.department == other.department and self.group == other.group and self.team == other.team
 
 # Define the EmployeeUpdate class
 class EmployeeUpdate:
@@ -27,21 +27,21 @@ def load_employee_list(fileName):
 		line_count = 0
 	
 		for row in csv_reader:
-			employee = Employee(row["id"], row["name"], row["department"], row["birthday month"])
+			employee = Employee(row["Employee Number"], row["Department"], row["Group"], row["Team"])
 			employeeList.append(employee)
 			
 			line_count += 1
 			
 	return employeeList
 
-# Function for getting a list of IDs from a list of Employee objects
-def get_employee_ids(employeeList):
-	employeeIDs = []
+# Function for getting a list of Employee Numbers from a list of Employee objects
+def get_employee_numbers(employeeList):
+	employeeNumbers = []
 	
 	for employee in employeeList:
-		employeeIDs.append(employee.id)
+		employeeNumbers.append(employee.employeeNumber)
 
-	return employeeIDs
+	return employeeNumbers
 
 
 ## EXECUTION
@@ -65,33 +65,33 @@ newFile = sys.argv[2]
 oldEmployeeList = load_employee_list(oldFile)
 newEmployeeList = load_employee_list(newFile)
 
-# Get old and new employee ID lists
-oldEmployeeIDs = get_employee_ids(oldEmployeeList)
-newEmployeeIDs = get_employee_ids(newEmployeeList)
+# Get old and new employee number lists
+oldEmployeeNumbers = get_employee_numbers(oldEmployeeList)
+newEmployeeNumbers = get_employee_numbers(newEmployeeList)
 
 # Find employees from old list who have been removed in new list
 for employee in oldEmployeeList:
-	if not (employee.id in newEmployeeIDs):
+	if not (employee.employeeNumber in newEmployeeNumbers):
 		removedEmployees.append(employee)
 	
 # Find employees from new list who were not in the old list OR are in the old list but are not an exact match
 for employee in newEmployeeList:
-	if not (employee.id in oldEmployeeIDs):
+	if not (employee.employeeNumber in oldEmployeeNumbers):
 		addedEmployees.append(employee)
 	else:
 		if not (employee in oldEmployeeList):
-			# Find the employee in the old employee list that has the same ID but is not identical, and note the changes
+			# Find the employee in the old employee list that has the same Employee Number but is not identical, and note the changes
 			updates = []
-			oldVersionsOfEmployee = filter(lambda x: x.id == employee.id, oldEmployeeList)
+			oldVersionsOfEmployee = filter(lambda x: x.employeeNumber == employee.employeeNumber, oldEmployeeList)
 			for oldVersionOfEmployee in oldVersionsOfEmployee:
-				if employee.name != oldVersionOfEmployee.name:
-					updates.append("Name: " + oldVersionOfEmployee.name + " → " + employee.name)
-				
 				if employee.department != oldVersionOfEmployee.department:
 					updates.append("Department: " + oldVersionOfEmployee.department + " → " + employee.department)
 				
-				if employee.birthMonth != oldVersionOfEmployee.birthMonth:
-					updates.append("Birth Month: " + oldVersionOfEmployee.birthMonth + " → " + employee.birthMonth)
+				if employee.group != oldVersionOfEmployee.group:
+					updates.append("Group: " + oldVersionOfEmployee.group + " → " + employee.group)
+				
+				if employee.team != oldVersionOfEmployee.team:
+					updates.append("Team: " + oldVersionOfEmployee.team + " → " + employee.team)
 			
 			employeeUpdate = EmployeeUpdate(employee, updates)	
 			employeeUpdates.append(employeeUpdate)
@@ -100,19 +100,19 @@ for employee in newEmployeeList:
 # Output the results
 print("The following employees were removed:")
 for employee in removedEmployees:
-	print(employee.name + " (ID: " + employee.id + ")")
+	print(employee.employeeNumber + " (Employee Number: " + employee.employeeNumber + ")")
 
 print("")
 
 print("The following employees were added:")
 for employee in addedEmployees:
-	print(employee.name + " (ID: " + employee.id + ")")
+	print(employee.employeeNumber + " (Employee Number: " + employee.employeeNumber + ")")
 	
 print("")
 
 print("The following employees were updated:")
 for employeeUpdate in employeeUpdates:
-	print(employeeUpdate.employee.name + " (ID: " + employee.id + ")")
+	print(employeeUpdate.employee.employeeNumber + " (Employee Number: " + employeeUpdate.employee.employeeNumber + ")")
 	
 	for update in employeeUpdate.updates:
 		print("\t" + update)
