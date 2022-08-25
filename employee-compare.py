790e9fdd10a54f0511d942e83579e6ab8aa43094
Brian Craine <briancraine@macbook-air.local>
2022-08-25 03:08:33 +0000

    Made columns configurable

--- employee-compare.py
+++ employee-compare.py
@@ -1,19 +1,21 @@
 import sys
 import csv
 
+# Configuration
+idField = "Employee Number"
+nameField = "Employee Name"
+otherFields = ["Department", "Group", "Team", "Supervisor", "Cost Center"]
+
+
 # Define the Employee class
 class Employee:
-	def __init__(self, employeeNumber, department, group, team, employeeName, supervisor, costCenter):
-		self.employeeNumber = employeeNumber
-		self.department = department
-		self.group = group
-		self.team = team
+	def __init__(self, employeeID, employeeName, employeeInfo):
+		self.employeeID = employeeID
 		self.employeeName = employeeName
-		self.supervisor = supervisor
-		self.costCenter = costCenter
+		self.employeeInfo = employeeInfo
 
 	def __eq__(self, other):
-		return self.employeeNumber == other.employeeNumber and self.department == other.department and self.group == other.group and self.team == other.team and self.employeeName == other.employeeName and self.supervisor == other.supervisor and self.costCenter == other.costCenter
+		return self.employeeID == other.employeeID and self.employeeName == other.employeeName and self.employeeInfo == other.employeeInfo
 
 # Define the EmployeeUpdate class
 class EmployeeUpdate:
@@ -30,7 +32,11 @@ def load_employee_list(fileName):
 		line_count = 0
 	
 		for row in csv_reader:
-			employee = Employee(row["Employee Number"], row["Department"], row["Group"], row["Team"], row["Employee Name"], row["Supervisor"], row["Cost Center"])
+			employeeInfo = {}
+			for field in otherFields:
+				employeeInfo[field] = row[field]
+			
+			employee = Employee(row[idField], row[nameField], employeeInfo)
 			employeeList.append(employee)
 			
 			line_count += 1
@@ -39,12 +45,12 @@ def load_employee_list(fileName):
 
 # Function for getting a list of Employee Numbers from a list of Employee objects
 def get_employee_numbers(employeeList):
-	employeeNumbers = []
+	employeeIDs = []
 	
 	for employee in employeeList:
-		employeeNumbers.append(employee.employeeNumber)
+		employeeIDs.append(employee.employeeID)
 
-	return employeeNumbers
+	return employeeIDs
 
 
 ## EXECUTION
@@ -69,42 +75,31 @@ oldEmployeeList = load_employee_list(oldFile)
 newEmployeeList = load_employee_list(newFile)
 
 # Get old and new employee number lists
-oldEmployeeNumbers = get_employee_numbers(oldEmployeeList)
-newEmployeeNumbers = get_employee_numbers(newEmployeeList)
+oldEmployeeIDs = get_employee_numbers(oldEmployeeList)
+newEmployeeIDs = get_employee_numbers(newEmployeeList)
 
 # Find employees from old list who have been removed in new list
 for employee in oldEmployeeList:
-	if not (employee.employeeNumber in newEmployeeNumbers):
+	if not (employee.employeeID in newEmployeeIDs):
 		removedEmployees.append(employee)
 	
 # Find employees from new list who were not in the old list OR are in the old list but are not an exact match
 for employee in newEmployeeList:
-	if not (employee.employeeNumber in oldEmployeeNumbers):
+	if not (employee.employeeID in oldEmployeeIDs):
 		addedEmployees.append(employee)
 	else:
 		if not (employee in oldEmployeeList):
 			# Find the employee in the old employee list that has the same Employee Number but is not identical, and note the changes
 			updates = []
-			oldVersionsOfEmployee = filter(lambda x: x.employeeNumber == employee.employeeNumber, oldEmployeeList)
+			oldVersionsOfEmployee = filter(lambda x: x.employeeID == employee.employeeID, oldEmployeeList)
 			for oldVersionOfEmployee in oldVersionsOfEmployee:
-				if employee.department != oldVersionOfEmployee.department:
-					updates.append("Department: " + oldVersionOfEmployee.department + " → " + employee.department)
-				
-				if employee.group != oldVersionOfEmployee.group:
-					updates.append("Group: " + oldVersionOfEmployee.group + " → " + employee.group)
-				
-				if employee.team != oldVersionOfEmployee.team:
-					updates.append("Team: " + oldVersionOfEmployee.team + " → " + employee.team)
-					
 				if employee.employeeName != oldVersionOfEmployee.employeeName:
-					updates.append("Employee Name: " + oldVersionOfEmployee.employeeName + " → " + employee.employeeName)
+					updates.append(nameField + ": " + oldVersionOfEmployee.employeeName + " → " + employee.employeeName)
 				
-				if employee.supervisor != oldVersionOfEmployee.supervisor:
-					updates.append("Supervisor: " + oldVersionOfEmployee.supervisor + " → " + employee.supervisor)
-					
-				if employee.costCenter != oldVersionOfEmployee.costCenter:
-					updates.append("Cost Center: " + oldVersionOfEmployee.costCenter + " → " + employee.costCenter)	
-			
+				for field in otherFields:
+					if employee.employeeInfo[field] != oldVersionOfEmployee.employeeInfo[field]:
+						updates.append(field + ": " + oldVersionOfEmployee.employeeInfo[field] + " → " + employee.employeeInfo[field])
+								
 			employeeUpdate = EmployeeUpdate(employee, updates)	
 			employeeUpdates.append(employeeUpdate)
 
@@ -112,19 +107,19 @@ for employee in newEmployeeList:
 # Output the results
 print("The following employees were removed:")
 for employee in removedEmployees:
-	print(employee.employeeName + " (Employee Number: " + employee.employeeNumber + ")")
+	print(employee.employeeName + " (Employee Number: " + employee.employeeID + ")")
 
 print("")
 
 print("The following employees were added:")
 for employee in addedEmployees:
-	print(employee.employeeName + " (Employee Number: " + employee.employeeNumber + ")")
+	print(employee.employeeName + " (Employee Number: " + employee.employeeID + ")")
 	
 print("")
 
 print("The following employees were updated:")
 for employeeUpdate in employeeUpdates:
-	print(employeeUpdate.employee.employeeName + " (Employee Number: " + employeeUpdate.employee.employeeNumber + ")")
+	print(employeeUpdate.employee.employeeName + " (Employee Number: " + employeeUpdate.employee.employeeID + ")")
 	
 	for update in employeeUpdate.updates:
 		print("\t" + update)
