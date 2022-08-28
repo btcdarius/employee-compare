463a69bb7ef084b0f4cefae413aba434b1b18977
Brian Craine <briancraine@macbook-air.local>
2022-08-28 14:32:03 +0000

    Moved excution to Main function

--- employee-compare.py
+++ employee-compare.py
@@ -54,74 +54,77 @@ def get_employee_numbers(employeeList):
 
 
 ## EXECUTION
+def main():
 
-# Create variables for output
-addedEmployees = []
-removedEmployees = []
-employeeUpdates = []
-
-
-# Exit with error if two arguments are not supplied
-if len(sys.argv) != 3:
-	print("Error: Two files must be supplied.")
-	sys.exit()
-
-# Get the old and new file paths from the arguments passed
-oldFile = sys.argv[1]
-newFile = sys.argv[2]
-
-# Load old and new employee lists into objects
-oldEmployeeList = load_employee_list(oldFile)
-newEmployeeList = load_employee_list(newFile)
-
-# Get old and new employee number lists
-oldEmployeeIDs = get_employee_numbers(oldEmployeeList)
-newEmployeeIDs = get_employee_numbers(newEmployeeList)
-
-# Find employees from old list who have been removed in new list
-for employee in oldEmployeeList:
-	if not (employee.employeeID in newEmployeeIDs):
-		removedEmployees.append(employee)
-	
-# Find employees from new list who were not in the old list OR are in the old list but are not an exact match
-for employee in newEmployeeList:
-	if not (employee.employeeID in oldEmployeeIDs):
-		addedEmployees.append(employee)
-	else:
-		if not (employee in oldEmployeeList):
-			# Find the employee in the old employee list that has the same Employee Number but is not identical, and note the changes
-			updates = []
-			oldVersionsOfEmployee = filter(lambda x: x.employeeID == employee.employeeID, oldEmployeeList)
-			for oldVersionOfEmployee in oldVersionsOfEmployee:
-				if employee.employeeName != oldVersionOfEmployee.employeeName:
-					updates.append(nameField + ": " + oldVersionOfEmployee.employeeName + " → " + employee.employeeName)
-				
-				for field in otherFields:
-					if employee.employeeInfo[field] != oldVersionOfEmployee.employeeInfo[field]:
-						updates.append(field + ": " + oldVersionOfEmployee.employeeInfo[field] + " → " + employee.employeeInfo[field])
-								
-			employeeUpdate = EmployeeUpdate(employee, updates)	
-			employeeUpdates.append(employeeUpdate)
-
-
-# Output the results
-print("The following employees were removed:")
-for employee in removedEmployees:
-	print(employee.employeeName + " (Employee Number: " + employee.employeeID + ")")
-
-print("")
-
-print("The following employees were added:")
-for employee in addedEmployees:
-	print(employee.employeeName + " (Employee Number: " + employee.employeeID + ")")
-	
-print("")
-
-print("The following employees were updated:")
-for employeeUpdate in employeeUpdates:
-	print(employeeUpdate.employee.employeeName + " (Employee Number: " + employeeUpdate.employee.employeeID + ")")
-	
-	for update in employeeUpdate.updates:
-		print("\t" + update)
+	# Create variables for output
+	addedEmployees = []
+	removedEmployees = []
+	employeeUpdates = []
 	
 	
+	# Exit with error if two arguments are not supplied
+	if len(sys.argv) != 3:
+		print("Error: Two files must be supplied.")
+		sys.exit()
+	
+	# Get the old and new file paths from the arguments passed
+	oldFile = sys.argv[1]
+	newFile = sys.argv[2]
+	
+	# Load old and new employee lists into objects
+	oldEmployeeList = load_employee_list(oldFile)
+	newEmployeeList = load_employee_list(newFile)
+	
+	# Get old and new employee number lists
+	oldEmployeeIDs = get_employee_numbers(oldEmployeeList)
+	newEmployeeIDs = get_employee_numbers(newEmployeeList)
+	
+	# Find employees from old list who have been removed in new list
+	for employee in oldEmployeeList:
+		if not (employee.employeeID in newEmployeeIDs):
+			removedEmployees.append(employee)
+		
+	# Find employees from new list who were not in the old list OR are in the old list but are not an exact match
+	for employee in newEmployeeList:
+		if not (employee.employeeID in oldEmployeeIDs):
+			addedEmployees.append(employee)
+		else:
+			if not (employee in oldEmployeeList):
+				# Find the employee in the old employee list that has the same Employee Number but is not identical, and note the changes
+				updates = []
+				oldVersionsOfEmployee = filter(lambda x: x.employeeID == employee.employeeID, oldEmployeeList)
+				for oldVersionOfEmployee in oldVersionsOfEmployee:
+					if employee.employeeName != oldVersionOfEmployee.employeeName:
+						updates.append(nameField + ": " + oldVersionOfEmployee.employeeName + " → " + employee.employeeName)
+					
+					for field in otherFields:
+						if employee.employeeInfo[field] != oldVersionOfEmployee.employeeInfo[field]:
+							updates.append(field + ": " + oldVersionOfEmployee.employeeInfo[field] + " → " + employee.employeeInfo[field])
+									
+				employeeUpdate = EmployeeUpdate(employee, updates)	
+				employeeUpdates.append(employeeUpdate)
+	
+	
+	# Output the results
+	print("The following employees were removed:")
+	for employee in removedEmployees:
+		print(employee.employeeName + " (Employee Number: " + employee.employeeID + ")")
+	
+	print("")
+	
+	print("The following employees were added:")
+	for employee in addedEmployees:
+		print(employee.employeeName + " (Employee Number: " + employee.employeeID + ")")
+		
+	print("")
+	
+	print("The following employees were updated:")
+	for employeeUpdate in employeeUpdates:
+		print(employeeUpdate.employee.employeeName + " (Employee Number: " + employeeUpdate.employee.employeeID + ")")
+		
+		for update in employeeUpdate.updates:
+			print("\t" + update)
+	
+	
+if __name__ == '__main__':
+		main()
